84ff4288f90788505e3206ef6e046ca66d7ace29
Toshiaki Ocho <bigbird19980311@gmail.com>
2021-06-08 19:12:10 +0900

    Bookモデルにカラムを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_08_075159) do
+ActiveRecord::Schema.define(version: 2021_06_08_075527) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "author"
+    t.text "comment"
+    t.string "group"
+    t.string "image"
+    t.string "isbn"
+    t.date "published_at"
+    t.string "publisher"
+    t.string "registrator"
+    t.string "status"
+    t.text "summary"
   end
 
   create_table "users", force: :cascade do |t|
--- test/models/book_test.rb
+++ test/models/book_test.rb
@@ -2,10 +2,20 @@
 #
 # Table name: books
 #
-#  id         :integer          not null, primary key
-#  title      :string
-#  created_at :datetime         not null
-#  updated_at :datetime         not null
+#  id           :integer          not null, primary key
+#  author       :string
+#  comment      :text
+#  group        :string
+#  image        :string
+#  isbn         :string
+#  published_at :date
+#  publisher    :string
+#  registrator  :string
+#  status       :string
+#  summary      :text
+#  title        :string
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
 #
 require "test_helper"
 
