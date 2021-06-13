81711281cfed9759511a4b58ea6709787adb737a
Toshiaki Ocho <bigbird19980311@gmail.com>
2021-06-13 18:35:24 +0900

    多分サインアップ画面でのusername入力欄を作った

--- config/routes.rb
+++ config/routes.rb
@@ -8,8 +8,5 @@ Rails.application.routes.draw do
   
   root to: "books#index"
   
-  devise_for :users, :controllers => {
-    :registrations => 'users/registrations',
-    :sessions => 'users/sessions'   
-  } 
+  devise_for :users
 end
