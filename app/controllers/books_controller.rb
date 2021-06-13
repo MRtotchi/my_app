5891b60111dec8bd15835945b7d65c9a5a9aaede
Toshiaki Ocho <bigbird19980311@gmail.com>
2021-06-14 02:02:53 +0900

    books,rentals,users_controllerの編集

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,66 @@
 class BooksController < ApplicationController
   def index
+    @books = Book.where(status: "購入済み").or(Book.where(status: ""))
+                 .page(params[:page]).per(10).reverse_order
   end
+
+  def show
+  end
+
+  def new
+  end
+
+  def edit
+  end
+
+  def create
+    @book = Book.new(book_params)
+
+    respond_to do |format|
+      if @book.save
+        format.html { redirect_to @book, notice: '書籍情報は正常に登録されました.' }
+        format.json { render :show, status: :created, location: @book }
+      else
+        format.html { render :new }
+        format.json { render json: @book.errors, status: :unprocessable_entity }
+      end
+    end     
+  end
+
+  def update
+    respond_to do |format|
+      if @book.update(book_params)
+        format.html { redirect_to @book, notice: '書籍情報は正常に更新されました.' }
+        format.json { render :show, status: :ok, location: @book }
+      else
+        format.html { render :edit }
+        format.json { render json: @book.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    @book.destroy
+    
+    respond_to do |format|
+      format.html { redirect_to books_url, notice: '書籍情報は正常に削除されました.' }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+
+    def set_book
+      @book = Book.find(:params[:id])
+    end
+
+    def book_params
+      params.require(:book)
+      .permit(
+              :title, :author, :image, :publisher, :published_at,
+              :comment, :category, :isbn, :language,
+              :price, :link, :regitrator, :reputation,
+              :status, :summary
+      ).merge(user_id: current_user.id)
+    end
 end
--- app/controllers/rentals_controller.rb
+++ app/controllers/rentals_controller.rb
@@ -1,3 +1,17 @@
 class RentalsController < ApplicationController
-  def
+  def borrow
+    rental = Rental.new(book_id: params[:id], user_id: current_user.id)
+    if rental.save
+      redirect_to books_path, notice: "書籍の貸し出し処理を行いました"
+    else
+      redirect_to book_path, notice: "書籍の貸し出し処理に失敗しました"
+    end
+  end
+
+  def return
+    rental = Rental.find(params[:id])
+    rental.update(returned: true)
+    redirect_to books_path, notice: "書籍の返却処理を行いました"
+  end
+
 end
